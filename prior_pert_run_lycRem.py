be8b92f133b93d01813e65b67884879913e6942f
Mark Paje <89901306+aliquid-novi@users.noreply.github.com>
2026-02-06 15:26:14 +1100

    Update prior_pert_run_lycRem.py

--- prior_pert_run_lycRem.py
+++ prior_pert_run_lycRem.py
@@ -102,10 +102,11 @@ for file in idata_data:
     case_deletion_runs = ppf.refit_case_deletion_grid(y, t_list, BASE_PRIORS, cleaned_name)
     
     print("Implementing observation pertubations...")
-    obs_shift_runs = ppf.refit_obs_shift_grid(y, cleaned_name, t_list=t_list, delta_list=delta_list, )
+    obs_shift_runs = ppf.refit_obs_shift_grid(y, cleaned_name, t_list=t_list[0:1], delta_list=delta_list, )
     
     print("Implementing prior pertubations...")
-    etas = [-0.5, -0.25, 0.0, 0.25, 0.5]
+    etas = [-0.5, -0.25, 0.25, 0.5]
     prior_runs_phi = ppf.refit_prior_perturbation_grid(y, etas, cleaned_name, scheme="phi_raw_scale", prefix="prior_pert")
+
 
 
